16c1ef6c0fb168f7c90e96df8a8894daf1cb8af4
macrob <epishkin.serg@gmail.com>
2025-03-25 15:27:07 +0000

    .

--- src/tpls/helpers/link.php
+++ src/tpls/helpers/link.php
@@ -2,8 +2,11 @@
 
 return function ($options) {
   $content = $options['fn']($this);
-  foreach($options['hash'] as  $key => $value) {
-    $content = str_replace(strtoupper($key), $value, $content);
+  
+  foreach ($options['hash'] as $key => $value) {
+    // Приводим $value к строке, если он null
+    $safeValue = $value ?? '';
+    $content = str_replace(strtoupper($key), $safeValue, $content);
   }
 
   return $content;
